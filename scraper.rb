82682a80f66e2ea8506b2a80db899e63b97d6960
Tony Bowden <tony@tmtm.com>
2015-05-15 08:09:36 +0100

    Make work for all terms

--- scraper.rb
+++ scraper.rb
@@ -18,18 +18,18 @@ end
 @WIKI = 'http://da.wikipedia.org'
 
 def wikilink(a)
-  return if a.attr('class') == 'new' 
+  return '' if a.attr('class') == 'new' 
   @WIKI + a['href']
 end
 
 @terms = {
-  # '1990' => 'Folketingsmedlemmer_valgt_i_1990',
-  # '1994' => 'Folketingsmedlemmer_valgt_i_1994',
-  # '1998' => 'Folketingsmedlemmer_valgt_i_1998',
-  # '2001' => 'Folketingsmedlemmer_valgt_i_2001',
-  # '2005' => 'Folketingsmedlemmer_valgt_i_2005',
+  '1990' => 'Folketingsmedlemmer_valgt_i_1990',
+  '1994' => 'Folketingsmedlemmer_valgt_i_1994',
+  '1998' => 'Folketingsmedlemmer_valgt_i_1998',
+  '2001' => 'Folketingsmedlemmer_valgt_i_2001',
+  '2005' => 'Folketingsmedlemmer_valgt_i_2005',
   '2007' => 'Folketingsmedlemmer_valgt_i_2007',
-  # '2011' => 'Folketingsmedlemmer_valgt_i_2011',
+  '2011' => 'Folketingsmedlemmer_valgt_i_2011',
 }
 
 @terms.each do |term, pagename|
@@ -41,20 +41,18 @@ end
     initial.css('li').each do |mem|
       data = { 
         name: mem.at_xpath('.//a').text.strip,
-        wikipedia: mem.at_xpath('.//a[not(@class="new")]/@href').text.strip,
+        wikipedia: wikilink(mem.at_xpath('.//a')),
         party: (mem.at_xpath('./text()').text.strip)[/\((.*?)\)/, 1],
         # constituency: district,
-        # source: url,
+        source: url,
         term: term,
       }
-      unless data[:party]
-        warn "No party for #{data[:name]}".red
-        next
-      end
+      next if %w(Indenrigsministeriet Folketinget.dk).include? data[:name]
+      raise "No party for #{data[:name]}".red unless data[:party]
       data[:wikipedia].prepend @WIKI unless data[:wikipedia].empty?
-      puts data
+      # puts data
       added += 1
-      # ScraperWiki.save_sqlite([:name, :term], data)
+      ScraperWiki.save_sqlite([:name, :term], data)
     end
   end
 
